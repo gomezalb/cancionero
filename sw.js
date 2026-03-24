c4a173164a53abc55660d67f9ab8bc942a600949
Alberto <adealbertogomez@hotmail.com>
2026-03-23 23:39:57 -0300

    funcionalidad de cambiar tamaño de fuente

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker del Cancionero
 // Versión del caché — cambiá este número cada vez que subas cambios a GitHub
-const CACHE_VERSION = "cancionero-v30";
+const CACHE_VERSION = "cancionero-v31";
 
 const ARCHIVOS = [
   "./",
