5d045aae2b2f125359b94ced1952337b3c2b3670
Alberto <adealbertogomez@hotmail.com>
2026-03-23 22:50:34 -0300

    Editar slides

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker del Cancionero
 // Versión del caché — cambiá este número cada vez que subas cambios a GitHub
-const CACHE_VERSION = "cancionero-v28";
+const CACHE_VERSION = "cancionero-v29";
 
 const ARCHIVOS = [
   "./",
