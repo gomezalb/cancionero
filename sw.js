24f1d61cbcd190f9652156e1daa637ca55063219
Alberto <adealbertogomez@hotmail.com>
2026-04-03 22:45:23 -0300

    cambio nro de version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker del Cancionero
 // Versión del caché — cambiá este número cada vez que subas cambios a GitHub
-const CACHE_VERSION = "cancionero-v49";
+const CACHE_VERSION = "cancionero-v50";
 
 const ARCHIVOS = [
   "./",
