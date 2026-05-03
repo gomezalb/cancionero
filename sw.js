9f6ead051adb24f0a78faa5e9d63f257990fe768
Alberto <adealbertogomez@hotmail.com>
2026-05-03 19:02:40 -0300

    más ajustes de tamaño para ganar espacio para las letras

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker del Cancionero
 // Versión del caché — cambiá este número cada vez que subas cambios a GitHub
-const CACHE_VERSION = "cancionero-v68";
+const CACHE_VERSION = "cancionero-v69";
 
 const ARCHIVOS = [
   "./",
