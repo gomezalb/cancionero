df72f29a38781ae581c8e092772623a1b9280779
Alberto <adealbertogomez@hotmail.com>
2026-05-09 12:26:21 -0300

    boton cambiar a mayúsculas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker del Cancionero
 // Versión del caché — cambiá este número cada vez que subas cambios a GitHub
-const CACHE_VERSION = "cancionero-v71";
+const CACHE_VERSION = "cancionero-v72";
 
 const ARCHIVOS = [
   "./",
